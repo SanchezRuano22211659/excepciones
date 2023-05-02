e8358925eeeb19e7ce18cca351ee5a263c206a25
Alexis Sánchez <126355162+SanchezRuano22211659@users.noreply.github.com>
2023-05-02 19:49:29 +0000

    initial program with 2 exceptiones

--- excepcionesprograma/Program.cs
+++ excepcionesprograma/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int? age = 0;
+        Console.WriteLine("Captura tu edad y presiona la tecla «ENTER»");
+        string? agecapture = Console.ReadLine();
+        if (agecapture is not null)
+        {
+            try
+            {
+                age = Convert.ToInt32(agecapture);
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Reingrese el resultado con un formato correcto");
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine("Reingrese el resultado con una edad válida");
+            }
+        }
+        if (age>=0)
+        {
+            Console.WriteLine($"edad: {age}");
+        }
+        else
+        {
+            Console.WriteLine("Reingrese el resultado con un número positivo");
+        }
+        
+    }
+}
